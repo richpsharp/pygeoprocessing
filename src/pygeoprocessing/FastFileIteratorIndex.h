51931365b5f3a9195c5d9b93240c528c28a14574
Rich Sharp <richpsharp@gmail.com>
2020-07-28 21:51:33 -0700

    try to debug if the index compare is working

--- src/pygeoprocessing/FastFileIteratorIndex.h
+++ src/pygeoprocessing/FastFileIteratorIndex.h
@@ -147,6 +147,7 @@ template <class DATA_T> class FastFileIteratorIndex{
 template <class DATA_T>
 int FastFileIteratorIndexCompare(FastFileIteratorIndex<DATA_T>* a,
                             FastFileIteratorIndex<DATA_T>* b) {
+    printf('a %f b %f', a->peek(), b->peek());
     return a->peek() < b->peek();
 }
 
